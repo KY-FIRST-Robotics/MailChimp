e0b4881795ebddb88932eb560eeb833bbedf0307
Gavin <gavin@kyfirstrobotics.org>
2025-07-25 11:50:25 -0400

    Set up split_name function, separates first and last name for MailChimp

--- csv_organizer.py
+++ csv_organizer.py
@@ -3,9 +3,14 @@ from tkinter import filedialog, messagebox
 import pandas as pd
 import os
 
+def split_name(name):
+    if pd.isna(name): return "", "" # Returns empty string if LC1, LC2, or Admin name is missing
+    parts = name.strip().split() # Separates first and last name, strips whitespace
+    return parts[0], " ".join(parts[1:]) if len(parts) > 1 else "" # Returns first and last name, returns last name as empty if there is only the first name
 
 def process_file(filepath):
     df = pd.read_csv(filepath, encoding="ISO-8859-1") # Initalizes DataFrame for working with spreadsheet, ensures file can be encoded and opened
+    active_teams = df[df["Active Team"].str.strip() == "Active"] # Filters out inactive teams
 
 def launch_gui():
     root = tk.Tk()
